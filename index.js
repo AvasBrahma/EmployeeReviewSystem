68137a787e81e8167ae51a8dbf77944d35f291d3
AvasBrahma <avasbrahma@gmail.com>
2023-06-16 23:12:23 +0530

    Created folder Structure

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const bodyParser = require('body-parser');
+const expressLayouts=require('express-ejs-layouts');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(express.static('./assets'));
 
-app.use(express.urlencoded());
 
 
 app.listen(port, function(err){
@@ -14,8 +18,12 @@ app.listen(port, function(err){
     console.log(`Server is running in port : ${port}`);
 });
 
+app.use(expressLayouts);
+
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 app.set('view engine', 'ejs')
 app.set('layout', './layouts/main');
+
+app.use('/',require('./routes'));
